686fd5960919bf6648c0ff78c1b71ecf6a820a9e
Adrien Navratil <adrien1975@live.fr>
2018-12-04 16:02:28 +0100

    Version increment

--- src/app/main.ts
+++ src/app/main.ts
@@ -20,9 +20,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 
-export const VERSION = 'Beta 1.4.1';
+export const VERSION = 'Beta 1.5.0';
 export const SERVER_URL = 'https://api.sakado.litarvan.com/';
-export const DEBUG = true;
+export const DEBUG = false;
 
 if (!DEBUG)
 {
